7be586a96cfa1af2995cbd78c1a3bcabb85016aa
Tahia Khan <tahia.khan@gmail.com>
2014-01-01 12:39:49 -0500

    Fixing syntax compile errors

--- opcodes.cpp
+++ opcodes.cpp
@@ -1,26 +1,26 @@
-#include "chip8.h"
+#include "chip-8.h"
 #include "defs.h"
 
 int Chip8::op_00E0() {	
 	/* Clears the screen */
-	while (int i = 0; i < DISPLAY_SIZE; i++) {
+	for (int i = 0; i < DISPLAY_SIZE; i++) {
 		graphics_[i] = 0;
 	}
 	display_updated_flag_ = true;
-	pc += 2;
+	pc_ += 2;
 	return SUCCESS;
 }
 
 int Chip8::op_1NNN() {
 	/* Jumps to address NNN */
-	pc = opcode_ & 0x0FFF;
+	pc_ = opcode_ & 0x0FFF;
 	return SUCCESS;
 }
 int Chip8::op_2NNN() {
 	/* Calls subroutine at NNN */
-	stack_[sp_] = pc;
+	stack_[sp_] = pc_;
 	sp_ += 1;
-	pc = opcode_ & 0x0FFF;
+	pc_ = opcode_ & 0x0FFF;
 	return SUCCESS;
 }
 
